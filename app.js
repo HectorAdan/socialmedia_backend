992ea7f4a60c140e87621b37cbedcd7db76db68d
HectorAdan <woliamatareyes@hotmail.com>
2021-09-28 01:57:57 -0500

    changes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const fileUpload = require('express-fileupload');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
 app.use(fileUpload());
 
